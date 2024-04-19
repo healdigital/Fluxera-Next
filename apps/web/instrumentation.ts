590c9322bfc47517c7655a7c65c4237de8ebd75d
giancarlo <giancarlopsk@gmail.com>
2024-04-19 17:21:41 +0800

    Move mock fetch function to web instrumentation
    The mock fetch function, which is used to patch the `fetch` function in the worker environment, has been moved from 'create-reader.ts' to 'instrumentation.ts'. This was necessary because the `cache` field is not implemented in the worker environment and was causing an error. This change ensures the CMS works properly in Cloudflare until support for the

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -4,6 +4,8 @@
  */
 
 export async function register() {
+  mockWorkerdFetch();
+
   // only run in nodejs runtime
   if (
     process.env.NEXT_RUNTIME === 'nodejs' &&
@@ -16,4 +18,40 @@ export async function register() {
     // Register monitoring instrumentation based on the MONITORING_PROVIDER environment variable.
     return registerMonitoringInstrumentation();
   }
+}
+
+/**
+ *
+ * Very ugly! But okay for the demo. Don't try this at home.
+ *
+ * This function is used to monkey patch the `fetch` function in the worker environment.
+ * It is necessary because the `cache` field is not implemented in the worker environment and will throw an error.
+ * TODO: remove once Cloudflare Workers supports the `cache` field.
+ *
+ * Without this - the CMS will not work in Cloudflare. And I really want the demo to work in Cloudflare.
+ *
+ * Based on: https://gist.github.com/tjenkinson/53b5724bf8112487f6bd797368e02b56
+ * Fixes: https://github.com/cloudflare/workerd/issues/698
+ */
+function mockWorkerdFetch() {
+  const originalFetch = globalThis.fetch;
+  globalThis.fetch = async function (...args) {
+    try {
+      return await originalFetch.apply(this, args);
+    } catch (e) {
+      if (!args[1] || typeof args[1] !== 'object') throw e;
+
+      const unimplementedCacheError =
+        e &&
+        typeof e === 'object' &&
+        'message' in e &&
+        e.message ===
+          "The 'cache' field on 'RequestInitializerDict' is not implemented.";
+      if (!unimplementedCacheError) throw e;
+
+      const newOpts = { ...args[1] };
+      delete newOpts.cache;
+      return originalFetch.apply(this, [args[0], newOpts]);
+    }
+  };
 }
